287e1ce160b216df6f85d4e384b86d8e08290931
Pedro Toscano <toscanopedroall@gmail.com>
2025-06-09 15:55:41 -0300

    chore: inicializa projeto Spring Boot com estrutura principal

---
+++ src/main/java/com/crud/eliel/CrudEliel1Application.java
@@ -0,0 +1,11 @@
+package com.crud.eliel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudEliel1Application {
+    public static void main(String[] args) {
+        SpringApplication.run(CrudEliel1Application.class, args);
+    }
+}
